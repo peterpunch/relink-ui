62f9733558b2d528de1ac499a690d33ac182bfa1
Tome Poljaric <tome.poljaric@icloud.com>
2025-07-15 17:41:14 +0200

    add some more error handling

--- src/RedirectShortLinkPage.tsx
+++ src/RedirectShortLinkPage.tsx
@@ -1,8 +1,10 @@
-import {type ReactElement, useEffect} from "react";
+import {type ReactElement, useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 
 export default function RedirectShortLinkPage(): ReactElement {
+    const [error, setError] = useState<string | undefined>(undefined);
     const {hash} = useParams()
+
     useEffect(() => {
         const requestOptions = {
             method: 'GET',
@@ -12,14 +14,28 @@ export default function RedirectShortLinkPage(): ReactElement {
         fetch(`http://localhost:8080/relink/api/short-links/${hash}`, requestOptions)
             .then(response => {
                 console.log(response)
+                if (response.status === 404) {
+                    setError("Short Link not found.")
+                    throw new Error("Short Link not found.");
+                }
                 return response.json()
             })
             .then(data => {
                 console.log(data)
                 window.location.href = data.url
             })
-            .catch((error) => console.error("Fetch error:", error));
+            .catch((error) => {
+                console.error("Fetch error:", error)
+                setError(error.toString())
+            });
 
     }, [hash])
-    return <h2>Hash: {hash}</h2>;
+    return <div>
+        <h2>{"http://localhost:5173/" + hash}</h2>
+        {error !== undefined && error.length > 0 && (
+            <div className="error-message">
+                <span>{error}</span>
+            </div>
+        )}
+    </div>;
 }
