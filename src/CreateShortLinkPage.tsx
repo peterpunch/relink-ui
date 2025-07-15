8538f414b7c77999c00a3b4e6637e188bfd363bc
Tome Poljaric <tome.poljaric@icloud.com>
2025-07-15 15:26:10 +0200

    add some error handling and validation

--- src/CreateShortLinkPage.tsx
+++ src/CreateShortLinkPage.tsx
@@ -1,15 +1,26 @@
 import {type ReactElement, type SetStateAction, useState} from "react";
 
+const urlRegexp = /https?:\/\/[^\s/$.?#].[^\s]*/;
 export default function CreateShortLinkPage(): ReactElement {
+
     const [inputValue, setInputValue] = useState<string>("");
+    const [error, setError] = useState<string | undefined>(undefined);
     const [hashValue, setHashValue] = useState<string | undefined>(undefined);
 
     const handleChange = (event: { target: { value: SetStateAction<string>; }; }) => {
         setInputValue(event.target.value);
+        setError(undefined)
     };
 
     const handleSubmit = async () => {
         console.log('handle submit')
+
+        if (!urlRegexp.test(inputValue)) {
+            setError(`${inputValue} is not a valid URL`)
+            setHashValue(undefined)
+            return false
+        }
+
         const requestOptions = {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
@@ -19,12 +30,18 @@ export default function CreateShortLinkPage(): ReactElement {
         fetch('http://localhost:8080/relink/api/short-links', requestOptions)
             .then(response => {
                 console.log(response)
+                if (response.status !== 201) {
+                    throw new Error("There is an internal error. Please try again later.")
+                }
                 return response.json()
             })
             .then(data => {
                 setHashValue(data.hash)
             })
-            .catch((error) => console.error("Fetch error:", error));
+            .catch((error) => {
+                console.error("Fetch error:", error)
+                setError(error.toString())
+            });
 
         return false
     }
@@ -46,6 +63,11 @@ export default function CreateShortLinkPage(): ReactElement {
                 <div className="output-element">
                     <span> <a href={"http://localhost:5173/" + hashValue}
                               target="_blank">http://localhost:5173/{hashValue}</a> </span>
+                </div>
+            )}
+            {error !== undefined && error.length > 0 && (
+                <div className="error-message">
+                    <span>{error}</span>
                 </div>
             )}
         </div>
